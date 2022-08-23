3c2e6588a3904ae416aa1426430accd602ba3e50
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-08-23 15:00:10 +0300

    initial

--- enrich_my/kafka_my/kafka_consumer_ext_class.py
+++ enrich_my/kafka_my/kafka_consumer_ext_class.py
@@ -1,45 +1,44 @@
 from confluent_kafka import Consumer
 from time import sleep
-# c.subscribe(['example_topic'])
 
-class KafkaConsumerMy(Consumer):
-    def __init__(self, topic):
-        self.consumer =  Consumer({
+class ExampleConsumer:
+    broker = "localhost:9092"
+    topic = "example_topic"
+    group_id = "mygroup"
+
+    def start_listener(self):
+        consumer_config = {
             'bootstrap.servers': 'localhost:9092',
             'group.id': 'mygroup',
             'auto.offset.reset': 'earliest'
-        })
+        }
 
-        self.consumer.subscribe(topic)
+        consumer = Consumer(consumer_config)
+        consumer.subscribe([self.topic])
 
-    def subscribe(self):
-        print(f' eto self.consumer : {self.consumer}')
-        return self.consumer
+        try:
+            while True:
+                print("Listening")
+                # read single message at a time
+                msg = consumer.poll(0)
 
+                if msg is None:
+                    sleep(5)
+                    continue
+                if msg.error():
+                    print("Error reading message : {}".format(msg.error()))
+                    continue
+                # You can parse message and save to data base here
+                print(msg)
+                consumer.commit()
 
+        except Exception as ex:
+            print("Kafka Exception : {}", ex)
 
+        finally:
+            print("closing consumer")
+            consumer.close()
 
-# RUNNING CONSUMER FOR READING MESSAGE FROM THE KAFKA TOPIC
-consumer = KafkaConsumerMy(['example_topic']).subscribe()
-try:
-    while True:
-        print("Listening")
-        # read single message at a time
-        msg = consumer.poll(0)
-
-        if msg is None:
-            sleep(5)
-            continue
-        if msg.error():
-            print("Error reading message : {}".format(msg.error()))
-            continue
-        # You can parse message and save to data base here
-        print(msg)
-        consumer.commit()
-
-except Exception as ex:
-    print("Kafka Exception : {}", ex)
-
-finally:
-    print("closing consumer")
-    consumer.close()
+#RUNNING CONSUMER FOR READING MESSAGE FROM THE KAFKA TOPIC
+my_consumer = ExampleConsumer()
+my_consumer.start_listener()
